bc44497e83778e4a877b0e5a2acf27b672d85c0f
pbullock08 <126013657+pbullock08@users.noreply.github.com>
2023-04-20 09:14:03 -0400

    finalized the code for setting the date and different classes to each timeblock depending on if it is past, present, or future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,12 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+var buttonEl = $('.saveBtn');
+var inputEl = $('textarea');
+var timeEl = $('.time-block');
+var today = dayjs();
+var date = $('#currentDay');
 
-
+// Wrap code in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the html
 $(function () {
-  var buttonEl = $('.saveBtn');
-  var timeEl = $('.time-block');
-  var today = dayjs();
-  var date = $('#currrentDay');
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -15,55 +14,51 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
 
-  // buttonEl.on("click", function(event){
-  //   console.log(event.target);
-  //   event.preventDefault();
+  buttonEl.on("click", function(event){
+    //console.log(event.target);
+    event.preventDefault();
 
+    $.each(inputEl, function(i, inputText) {
+      
+    });
+
+  }); 
+    
+  //inputText.value;
   // // var toDo = $()
 
-// }); 
-
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
+  //code to apply the past, present, or future class to each time block by comparing the id to the current hour
   function setClass() {
     $.each(timeEl, function (i, timeBlock) {
-      if (timeBlock.attributes[0] == today.hour()) {
-        $('.time-block').addClass('present');
-      } else if (timeBlock.attributes[0] < today.hour()) {
-        $('time.block').addClass('past');
+      if (parseInt(timeBlock.attributes[0].value) === today.$H) {
+        timeBlock.classList.add('present');
+      } else if (parseInt(timeBlock.attributes[0].value) < today.$H) {
+        timeBlock.classList.add('past');
       } else {
-        $('time-block').addClass('future');
+        timeBlock.classList.add('future');
       }
     });
   }
 
   setClass();
 
-
-// function setClass() {
-//   $.each(timeEl, function (i, timeBlock) {
-//     if ($('div').attr('id') == "hour-" + today.hour()) {
-//       timeBlock.addClass('present');
-//     } else if ($('div').attr('id') < "hour-" + today.hour()) {
-//       timeBlock.addClass('past');
-//     } else {
-//       timeBlock.addClass('future');
-//     }
-//   });
-// }
-  //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 
-  
-  date.text(today.format('dddd, MMMM D'));
 
+  // code to display the current date in the header of the page
+  function setDate() {
+    if (today.$D === 4 ||today.$D === 5 || today.$D === 6 || today.$D === 7 || today.$D === 8 || today.$D === 9 || today.$D === 10 || today.$D === 11 || today.$D === 12 || today.$D === 13 || today.$D === 14 || today.$D === 15 || today.$D === 16 || today.$D === 17 || today.$D === 18 || today.$D === 19 || today.$D === 20 || today.$D === 24 || today.$D === 25 || today.$D === 26 || today.$D === 27 || today.$D === 28 || today.$D === 29 || today.$D === 30) {
+      date.text(today.format('dddd, MMMM D') + 'th')
+    }  else if (today.$D === 1 ||  today.$D === 21 || today.$D === 31) {
+      date.text(today.format('dddd, MMMM D') + 'st')
+    } else if (today.$D === 2 || today.$D === 22) {
+      date.text(today.format('dddd, MMMM D') + 'nd')
+    } else {
+      date.text(today.format('dddd, MMMM D') + 'rd')
+    }
+  } 
+
+  setDate();
 });
